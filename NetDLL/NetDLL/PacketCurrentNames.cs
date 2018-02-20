75c3930d771662fe3c32471d27dd9f9e24f55cb2
MarkusStahlmann <markusstahl99@gmail.com>
2018-02-20 08:07:32 +0100

    Add All Packets

---
+++ NetDLL/NetDLL/PacketCurrentNames.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NetDLL
+{
+    [Serializable]
+    public class PacketCurrentNames : Packet
+    {
+        public List<string> Names { get; private set; }
+
+        public PacketCurrentNames(string[] names)
+        {
+            Names = names.ToList();
+        }
+    }
+}
